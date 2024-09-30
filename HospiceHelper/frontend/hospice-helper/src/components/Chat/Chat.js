c1f3dd2307fd78bcde1fdf9807da177ea394938c
Uthman Alibalogun <121103811+uthmanoali98@users.noreply.github.com>
2024-09-30 11:06:26 -0700

    Update Chat.js

--- HospiceHelper/frontend/hospice-helper/src/components/Chat/Chat.js
+++ HospiceHelper/frontend/hospice-helper/src/components/Chat/Chat.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from '../../axios';
 import './Chat.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane, faMicrophone } from '@fortawesome/free-solid-svg-icons';
